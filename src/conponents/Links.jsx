2a48c1fdee7bc74fd778ef066f71b2eb3dc25058
Promise Benard Augustine <promisebenard1@gmail.com>
2022-10-29 15:25:13 +0100

    feature: added the subtext to the link components

--- src/conponents/Links.jsx
+++ src/conponents/Links.jsx
@@ -1,17 +1,16 @@
 import { clsx } from "clsx";
+import ReactTooltip from "react-tooltip";
 
-const Links = ({ url, children, id, className }) => {
+const Links = ({ url, children, id, className, subText }) => {
   return (
-    <a
-      target="_blank"
-      href={url}
-      id={id}
-      className={clsx("w-full max-w-[1000px]", className)}
-    >
-      <button className=" py-4 rounded-lg text-center bg-gray-200 w-full md:text-lg font-medium text-black hover:bg-gray-400 transition-colors delay-200 ease-in ">
-        {children}
-      </button>
-    </a>
+    <div className={clsx("w-full max-w-[1000px] relative group", className)}>
+      <a data-tip={subText} target="_blank" href={url} id={id}>
+        <button className=" py-4 rounded-lg text-center bg-gray-200 w-full md:text-lg font-medium text-black hover:bg-gray-400 transition-colors delay-200 ease-in ">
+          {children}
+        </button>
+        <ReactTooltip />
+      </a>
+    </div>
   );
 };
 
--- src/conponents/LinksCard.jsx
+++ src/conponents/LinksCard.jsx
@@ -3,28 +3,47 @@ import Links from "./Links";
 const LinksCard = () => {
   return (
     <div className="w-full flex flex-col gap-4 md:gap-10 items-center justify-center">
-      <Links url="/#">thepromisebenard.com (comming soon)</Links>
+      <Links subText="Link to my upcoming portfolio website" url="/#">
+        thepromisebenard.com (comming soon)
+      </Links>
 
-      <Links url="https://training.zuri.team/" id="btn__zuri">
+      <Links
+        subText="Meet the wonderful Zuri team"
+        url="https://training.zuri.team/"
+        id="btn__zuri"
+      >
         Zuri's Team
       </Links>
 
-      <Links url="http://books.zuri.team " id="books">
+      <Links
+        subText="Take a tour of Zuri's Book store"
+        url="http://books.zuri.team "
+        id="books"
+      >
         Zuri's Books
       </Links>
 
       <Links
+        subText="Grab a copy of my best selling beginner's friendly python book"
         url="https://books.zuri.team/python-for-beginners?ref_id=thepromisebenard"
         id="book__python"
       >
-        Best Python Books
+        Best Python Book
       </Links>
 
-      <Links url="https://background.zuri.team" id="pitch">
+      <Links
+        subText="If you have been looking for how to put your tech skills to test, Then sign up for Talent Hunt"
+        url="https://background.zuri.team"
+        id="pitch"
+      >
         Talent Hunt
       </Links>
 
-      <Links url="https://books.zuri.team/design-rules" id="book__design">
+      <Links
+        subText="Zuri's design book would get you up to speed. Grab a copy NOW!"
+        url="https://books.zuri.team/design-rules"
+        id="book__design"
+      >
         Zuri's Design Book
       </Links>
     </div>
