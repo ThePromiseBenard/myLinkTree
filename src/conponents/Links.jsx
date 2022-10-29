e3fb435c2f8512aa8227df12716ec891602cc270
Promise Benard Augustine <promisebenard1@gmail.com>
2022-10-29 15:27:16 +0100

    fix: fixed the bug with the modals and subtext

--- src/conponents/Links.jsx
+++ src/conponents/Links.jsx
@@ -3,7 +3,7 @@ import ReactTooltip from "react-tooltip";
 
 const Links = ({ url, children, id, className, subText }) => {
   return (
-    <div className={clsx("w-full max-w-[1000px] relative group", className)}>
+    <div className={clsx("w-full max-w-[1000px]", className)}>
       <a data-tip={subText} target="_blank" href={url} id={id}>
         <button className=" py-4 rounded-lg text-center bg-gray-200 w-full md:text-lg font-medium text-black hover:bg-gray-400 transition-colors delay-200 ease-in ">
           {children}
